eeefc356dc506afbf7b962e9b27df59d7dfaf7db
TrenerGP <kamieniarz.work@gmail.com>
2025-06-29 11:37:42 +0200

    Lekcja41

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+    public ColorToPrefab[] colorMappings;
+    public float offset = 5f;
+
+    void GenerateTile(int x, int z)
+    {
+        Color pixelColor = map.GetPixel(x, z);
+        if (pixelColor.a == 0) return;
+        foreach (var c in colorMappings)
+        {
+            if (c.color.Equals(pixelColor))
+            {
+                Vector3 pos = new Vector3(x, 0, z) * offset;
+                Instantiate(c.prefab, pos, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    void GenerateLabirynth()
+    {
+        for (int x=0; x<map.width; x++)
+        {
+            for (int z=0; z<map.height; z++)
+            {
+                GenerateTile(x, z);
+            }
+        }
+    }
+}
--- Assets/Scripts/Lock.cs
+++ Assets/Scripts/Lock.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 public class Lock : MonoBehaviour
 {
+    public Door[] doors;
+    public KeyColor myColor;
+    private bool unlocked;
     [SerializeField] private bool canOpen = false;
+    Animator key;
+
+    private void Start()
+    {
+        key = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -20,5 +29,45 @@ public class Lock : MonoBehaviour
             canOpen = false;
             Debug.Log($"Door canOpen={canOpen}");
         }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canOpen && !unlocked)
+        {
+            key.SetBool("useKey", CheckKey());
+        }
+    }
+
+    public void OpenDoors()
+    {
+        foreach (Door d in doors)
+        {
+            d.OpenDoor();
+        }
+    }
+
+    public bool CheckKey()
+    {
+        if(GameManager.gameManager.redKey>0 && myColor==KeyColor.Red)
+        {
+            GameManager.gameManager.redKey--;
+            unlocked = true;
+            return true;
+        }
+        if (GameManager.gameManager.greenKey > 0 && myColor == KeyColor.Green)
+        {
+            GameManager.gameManager.greenKey--;
+            unlocked = true;
+            return true;
+        }
+        if (GameManager.gameManager.blueKey > 0 && myColor == KeyColor.Blue)
+        {
+            GameManager.gameManager.blueKey--;
+            unlocked = true;
+            return true;
+        }
+        Debug.Log("You don't have a key!");
+        return false;
     }
 }
